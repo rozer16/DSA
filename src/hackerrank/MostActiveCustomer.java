d106151e4bf01824655e963919e57e0ea4296ffe
Ravi Malkan <ravimalkan@gmail.com>
2022-04-18 00:12:21 +0530

    Changes

---
+++ src/hackerrank/MostActiveCustomer.java
@@ -0,0 +1,51 @@
+package hackerrank;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class MostActiveCustomer {
+
+    /*
+     * Complete the 'mostActive' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts STRING_ARRAY customers as parameter.
+     */
+
+    public static List<String> mostActive(List<String> customers) {
+    	Map<String, Long> counts =
+    			customers.stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+    	List<String> result = new ArrayList();
+    	
+    	float percentage = (float)customers.size()*(5.0f/100.0f);
+    	for(String s:counts.keySet()) {
+    		if((float)counts.get(s) >= percentage) {
+    			result.add(s);
+    		}
+    	}
+    	Collections.sort(result);
+    	return result;
+    }
+    public static void main(String[] args) throws IOException {
+        
+        int customersCount =20;
+        String [] aa = {"Alpha","Beta","Zeta","Beta","Zeta","Zeta","Epsilon","Beta","Zeta","Beta","Zeta","Beta","Delta","Zeta","Beta","Zeta","Beta","Zeta","Beta","Zeta","Beta"};
+        List<String> customers = Arrays.asList(aa);
+
+        List<String> result = MostActiveCustomer.mostActive(customers);
+
+       System.out.println(result.stream().collect(joining("\n"))+ "\n");
+    }
+
+}
---
+++ src/string/SeprateStringWithSameCharOccurance.java
@@ -0,0 +1,43 @@
+package string;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SeprateStringWithSameCharOccurance {
+	
+	public static void main(String[] args) {
+		  List<String> strs = Arrays.asList("eat", "tea", "tan", "ate", "nat", "bat");
+	      System.out.println(getStringList(strs));
+	}
+	private static String sortString(String str){
+		char [] chars = str.toCharArray();
+		Arrays.sort(chars);
+		return String.copyValueOf(chars);
+	}
+	private static List<List<String>> getStringList(List<String> strList){
+		Map<String,List<String>> map = new HashMap();
+
+		strList.forEach(s->{
+			System.out.println("Processing :"+s);
+			String sortedString = sortString(s);
+			if(map.containsKey(sortedString)) {
+				map.get(sortedString).add(s);
+			}else {
+				List<String> strings = new ArrayList<>();
+				strings.add(s);
+				map.put(sortedString, strings);
+			}
+
+			System.out.println(map);
+		});
+		List<List<String>> strs = new ArrayList<List<String>>();
+		map.values().forEach(list -> {
+			Collections.sort(list);
+			strs.add(list);
+		});
+		return strs;
+	}
+}
