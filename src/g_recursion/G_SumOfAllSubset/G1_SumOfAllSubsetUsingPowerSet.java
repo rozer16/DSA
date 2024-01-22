9c0752aad5873dc4806034c1796018affef62ba2
ravimalkan <ravimalkan@gmail.com>
2024-01-22 16:49:02 +0530

    Changes

--- src/g_recursion/G_SumOfAllSubset/G1_SumOfAllSubsetUsingPowerSet.java
+++ src/g_recursion/G_SumOfAllSubset/G1_SumOfAllSubsetUsingPowerSet.java
@@ -17,24 +17,18 @@ https://youtu.be/rYkfBRtMJr8
 Examples:
 
 Example 1:
+* s = "abc"
+* All substrings : "" a b c ab bc ac abc
+* no of possible substring : 2^n :8
+* 0  000
+* 1  001
+* 2  010
+* 3  011
+* 4  100
+* 5  101
+* 6  110
+* 7  111
 
-Input: N = 3, arr[] = {5,2,1}
-
-Output: 0,1,2,3,5,6,7,8
-
-Explanation: We have to find all the subset’s sum and print them in ascending order.
-* in this case the generated subsets are [ [], [1], [2], [2,1], [5], [5,1], [5,2]. [5,2,1],
-* so the sums we get will be  0,1,2,3,5,6,7,8
-
-
-Input: N=3,arr[]= {3,1,2}
-
-Output: 0,1,2,3,3,4,5,6
-
-Explanation: We have to find all the subset’s sum and print them in ascending order.
- in this case the generated subsets are [ [], [1], [2], [2,1], [3], [3,1], [3,2]. [3,2,1],
-so the sums we get will be  0,1,2,3,3,4,5,6
-*
 *
 * */
 public class G1_SumOfAllSubsetUsingPowerSet {
