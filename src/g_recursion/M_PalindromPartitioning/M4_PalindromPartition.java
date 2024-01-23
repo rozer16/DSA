d6c776c5a6b4ceaae5f79a47d1f5b546cac4a854
ravimalkan <ravimalkan@gmail.com>
2024-01-24 01:07:08 +0530

    Changes

--- src/g_recursion/M_PalindromPartitioning/M4_PalindromPartition.java
+++ src/g_recursion/M_PalindromPartitioning/M4_PalindromPartition.java
@@ -17,18 +17,18 @@ public class M4_PalindromPartition {
     }
 
     private static void func(int index, String s, List<String> path, List<List<String>> res) {
-        if(index == s.length()){ //If partition has reached last index that means all prev partitions had palindrome str
+        if (index == s.length()) { //If partition has reached last index that means all prev partitions had palindrome str
             res.add(new ArrayList<>(path));
             return;
         }
         for (int i = index; i < s.length(); ++i) { //Started i with index so that first character itself is always palindrome
-            if(isPalindrome(s,index,i)){ //Check if index to i is palindrome
-                path.add(s.substring(index,i+1)); //If index to i is palindrome then add to temp result
-                func(i+1,s,path,res); //Call recursion function for i+1
-                path.remove(path.size()-1); //Backtracking remove temp string what we have removed            }
+            if (isPalindrome(s, index, i)) { //Check if index to i is palindrome
+                path.add(s.substring(index, i + 1)); //If index to i is palindrome then add to temp result
+                func(i + 1, s, path, res); //Call recursion function for i+1
+                path.remove(path.size() - 1); //Backtracking remove temp string what we have removed            }
+            }
         }
     }
-
     private static boolean isPalindrome(String str, int start, int end) {
         while(start <= end){
             if(str.charAt(start++) != str.charAt(end--))
