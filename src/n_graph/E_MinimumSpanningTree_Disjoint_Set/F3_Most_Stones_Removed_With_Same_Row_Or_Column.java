803d66b788e0da030455e43c9750e7fdc7d89b42
ravimalkan <ravimalkan@gmail.com>
2024-12-30 20:13:23 +0530

    Changes

--- src/n_graph/E_MinimumSpanningTree_Disjoint_Set/F3_Most_Stones_Removed_With_Same_Row_Or_Column.java
+++ src/n_graph/E_MinimumSpanningTree_Disjoint_Set/F3_Most_Stones_Removed_With_Same_Row_Or_Column.java
@@ -18,6 +18,16 @@ public class F3_Most_Stones_Removed_With_Same_Row_Or_Column {
 
     }
 
+
+    /*
+    Time Complexity: O(N), where N = total no. of stones.
+                    Here we have just traversed the given stones array several times.
+                    And inside those loops, every operation is apparently taking constant time.
+                    So, the time complexity is only the time of traversal of the array.
+
+    Space Complexity: O(2* (max row index + max column index))
+    for the parent and size array inside the Disjoint Set data structure.
+    * */
     public int removeStones(int[][] stones) {
         int noOfRows = -1;
         int noOfCols = -1;
@@ -31,6 +41,7 @@ public class F3_Most_Stones_Removed_With_Same_Row_Or_Column {
         Set<Integer> nodes = new HashSet<>();
         C3_Disjointset_Union_By_Size ds = new C3_Disjointset_Union_By_Size(noOfRows+noOfCols+1);
         for (int i = 0; i < stones.length; i++) {
+            //Considering each row as node and each col as separate node.
             int rowNode = stones[i][0];
             int colNode = stones[i][1] + noOfRows+1;
 
